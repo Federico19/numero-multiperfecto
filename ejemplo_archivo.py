c9ccb83cb7c8b1b317fcf375ed1869c64d9f9d28
Federico Ocampo <30559203+Federico19@users.noreply.github.com>
2023-04-19 23:33:51 -0300

    Codigo que indica si un numero es multi-perfecto, archivos .py del profe agregados

---
+++ ejemplo_archivo.py
@@ -0,0 +1,13 @@
+from decorators import delta_time
+
+
+@delta_time("GRUPO GN")
+def multiperfecto(n):
+	####
+	####
+	s()
+
+	return lista
+
+if __name__ == "__main__":
+	print(multiperfecto(100))
--- main.py
+++ main.py
@@ -1,13 +1,27 @@
-def divisores(n):
-  lista=[]
-  for i in range(1, n+1):
-    if (n % i) == 0:
-      lista.append(i)
+def divisores(numero):
+  #Tanto 1 como el mismo numero son divisores de numero
+  lista_divisores=[1,numero]
+  #Los divisores de cualquier numero se encuentran entre 1 y el numero dividido 2, a excepcion del mismo numero
+  for i in range(2, numero//2 +1):
+    if (numero % i) == 0:
+      lista_divisores.append(i)
 
-  return lista
+  return lista_divisores
 
-def sumaDivisores(lista):
-  
+def sumaDivisores(lista_divisores):
+  resultado = 0
+  for numero in lista_divisores:
+    resultado += numero
+  return resultado
 
-divisores = divisores(6)
-print(divisores)
+def es_multiperfecto(numero, suma_divisores):
+  #Tomamos que parta del 6 en adelante ya que este es el primer numero multiperfecto.
+  if (numero >5 and suma_divisores%numero == 0 and suma_divisores//numero >1):
+    return True
+  return False
+
+total = []
+for i in range(1,8130):
+  if(es_multiperfecto(i,sumaDivisores(divisores(i)))):
+    total.append(i)
+print(total)
