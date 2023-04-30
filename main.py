8bdc928c7b75857248efd66fa1704774a7b9e67f
Nilson Orrego <nilson.nehuen@ca.frre.utn.edu.ar>
2023-04-30 17:07:21 -0300

    último cambio

--- main.py
+++ main.py
@@ -1,38 +1,40 @@
 from decorators import delta_time
 
-#@delta_time("GRUPO G4")
-def factores_primos(numero, contador):
+def factores_primos(numero):
+  return factores_primos_aux(numero,[] , 2)
+    
+def factores_primos_aux(numero, list, contador):
   if numero == 1:
-    return []
+    return list
   else:
     if numero % contador == 0:
-      return [contador] + factores_primos(numero // contador, contador)
+      return factores_primos_aux(numero // contador, (list + [contador]), contador)
     else:
-      return factores_primos(numero,contador+1)
-                             
-lista2 = factores_primos(6120,2)
-lista = []
-lista = dict(zip(lista2,map(lambda x: lista2.count(x),lista2)))
+      return factores_primos_aux(numero + 0, list + [], contador+1)
+    
 
-productoria= 1
+def productoria (num):
+  lista = {}
+  lista = dict(zip(num,map(lambda x: num.count(x),num)))
 
-for clave in lista:
-  productoria = productoria * ((clave ** (lista [clave] + 1)-1)/(clave - 1))
+  resultado= 1
+
+  for clave in lista:
+    resultado = resultado * ((clave ** (lista [clave] + 1)-1)/(clave - 1))
+  return resultado
+
+@delta_time("GRUPO G4")
+def multiperfectos(rango):
+    lista_multiperfectos = []
+    for numero in range (6,rango):
+        fac_prim = factores_primos (numero)
+        sum = productoria (fac_prim)
+        if (numero >5 and sum%numero == 0 and sum//numero >1):
+          lista_multiperfectos.append(numero)
+    return lista_multiperfectos
 
 
-def es_multiperfecto(numero, suma_divisores):
-  #Tomamos que parta del 6 en adelante ya que este es el primer numero multiperfecto.
-  #Tomamos que suma_divisores//numero >1 ya que k debe ser >=2
-  if (numero >5 and suma_divisores%numero == 0 and suma_divisores//numero >1):
-    return True
-  return False
-
-def lista_multiperfectos(rango):
-	total = []
-	for i in range(rango):
-		if(es_multiperfecto(i,productoria)):
-			total.append(i)
-	return total
-
+""" tope = int(input("Numero: "))
 if __name__ == "__main__":
-	print(lista_multiperfectos(120))
+	print(multiperfectos(tope)) """
+print(multiperfectos(100000));
