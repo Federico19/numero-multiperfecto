524d853d96d550d5507c65c63c58bc680501e95a
Federico Ocampo <30559203+Federico19@users.noreply.github.com>
2023-04-30 18:35:47 -0300

    Archivos.py actualizados

--- factorizacion.py
+++ factorizacion.py
@@ -1,55 +1,47 @@
+#Calculo de numeros multiperfectos mediante sus factores primos.
 from decorators import delta_time
+import sys
+sys.setrecursionlimit(100000)
 
 @delta_time("GRUPO G4")
 def multiperfecto(rango):
-	return lista_multiperfectos(rango)
+	return multiperfectos(rango)
 
 def divisores(n):
-    # factorización prima
-    factors = {}
-    d = 2
-    while d * d <= n:
-        while (n % d) == 0:
-            if d not in factors:
-                factors[d] = 0
-            factors[d] += 1
-            n //= d
-        d += 1
-    if n > 1:
-        if n not in factors:
-            factors[n] = 0
-        factors[n] += 1
+  # factorización prima
+  factors = {}
+  d = 2
+  while d * d <= n:
+      while (n % d) == 0:
+          if d not in factors:
+              factors[d] = 0
+          factors[d] += 1
+          n //= d
+      d += 1
+  if n > 1:
+      if n not in factors:
+          factors[n] = 0
+      factors[n] += 1
+  return factors
 
-    # construir la lista de divisores a partir de los factores primos
-    divisors = [1]
-    for prime in factors:
-        temp_divisors = []
-        for i in range(factors[prime] + 1):
-            for divisor in divisors:
-                temp_divisors.append(divisor * (prime ** i))
-        divisors = temp_divisors
-
-    return divisors
-
-def sumaDivisores(lista_divisores):
-  resultado = 0
-  for numero in lista_divisores:
-    resultado += numero
-  return resultado
-
-def es_multiperfecto(numero, suma_divisores):
-  #Tomamos que parta del 6 en adelante ya que este es el primer numero multiperfecto.
-  #Tomamos que suma_divisores//numero >1 ya que k debe ser >=2
-  if (numero >5 and suma_divisores%numero == 0 and suma_divisores//numero >1):
-    return True
-  return False
-
-def lista_multiperfectos(rango):
-	total = []
-	for i in range(rango):
-		if(es_multiperfecto(i,sumaDivisores(divisores(i)))):
-			total.append(i)
-	return total
+def suma_divisores(factores):
+  if len(factores) == 1:
+    base = next(iter(factores.keys()))
+    exponente = factores[base]
+    return (base**(exponente+1) -1) // (base-1)
+  else:
+    base = list(factores.keys())[-1]
+    exponente = factores[base]
+    del factores[base]
+    return ((base**(exponente+1) -1) // (base-1)) * suma_divisores(factores)
+  
+def multiperfectos(rango):
+  lista_multiperfectos = []
+  for numero in range(6,rango+1):
+    sumatoria = suma_divisores(divisores(numero))
+    if( sumatoria % numero == 0 and sumatoria//numero >1):
+      lista_multiperfectos.append(numero)
+  return lista_multiperfectos
 
 if __name__ == "__main__":
 	print(multiperfecto(1000000))
--- multiperfectos.py
+++ multiperfectos.py
@@ -1,4 +1,6 @@
-#Calculo de numeros multiperfectos mediante sus factores y en forma recursiva
+#Calculo de numeros multiperfectos mediante sus factores primos y en forma recursiva.
+#NO FUNKAAA! Se rompe la funcion factores_primos_diccionario al iterar en un for con rango>2400
+
 from decorators import delta_time
 import sys
 sys.setrecursionlimit(100000)
@@ -14,37 +16,26 @@ def factores_primos_diccionario(numero, contador, diccionario):
       return factores_primos_diccionario(numero // contador, contador, diccionario)
     else:
       return factores_primos_diccionario(numero, contador +1, diccionario)
-    
+
 def suma_divisores(factores):
-  """  if len(factores) == 1:
-      base = next(iter(factores.keys()))
-      exponente = factores[base]
-      return (base**(exponente+1) -1) // (base-1)
-    else:
-      base = list(factores.keys())[-1]
-      exponente = factores[base]
-      del factores[base]
-      return ((base**(exponente+1) -1) // (base-1)) * suma_divisores(factores)"""
-  sumatoria = 1
-  while len(factores)<1:
-      base = list(factores.keys())[-1]
-      exponente = factores[base]
-      sumatoria *= (base**(exponente+1) -1) // (base-1)
-  return sumatoria
-  
+  if len(factores) == 1:
+    base = next(iter(factores.keys()))
+    exponente = factores[base]
+    return (base**(exponente+1) -1) // (base-1)
+  else:
+    base = list(factores.keys())[-1]
+    exponente = factores[base]
+    del factores[base]
+    return ((base**(exponente+1) -1) // (base-1)) * suma_divisores(factores)
   
 def multiperfectos(rango):
   lista_multiperfectos = []
   for numero in range(6,rango+1):
-    """print(numero)
-
-      if( sumatoria % numero == 0 and sumatoria//numero >1):
-        lista_multiperfectos.append(numero)
-        """
-    diccionario = factores_primos_diccionario(numero,2,dict())
-    #sumatoria = suma_divisores(diccionario)
-    print(numero)
-    print(diccionario)
+    sumatoria = suma_divisores(factores_primos_diccionario(numero,2, dict()))
+    if(sumatoria % numero == 0 and sumatoria//numero >1):
+      lista_multiperfectos.append(numero)
+      print(numero)
   return lista_multiperfectos
 
-print(multiperfectos(1000000))
+
+print(multiperfectos(10000))
